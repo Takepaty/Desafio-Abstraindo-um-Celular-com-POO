f78926d3215efe4125cdd4594424ebc07fa06c0f
Patricia Takehara <paty.takeharab@outlook.com>
2022-12-21 15:41:04 -0300

    Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using AbstraindoumCelularcomPOO.Models;
+
+Console.WriteLine("Teste Nokia");
+Smartphone nokia = new Nokia("(11) 92222-2222", "Uia4", "123123123", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("LinkedIn");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Teste iPhone");
+Smartphone iphone = new Iphone("(11) 93333-3333", "Modelo Top", "321321321", 185);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
